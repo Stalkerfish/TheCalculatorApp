b07c022201d56bc2dbbb3e3db2881dc852e44b34
Allison Silva <silva.allison2002@gmail.com>
2022-12-12 13:15:08 -0300

    Starting to give life to the buttons and appending to the top textview messing up with the 'append' method

--- app/src/main/java/com/stalkerfish/thecalculator/MainActivity.kt
+++ app/src/main/java/com/stalkerfish/thecalculator/MainActivity.kt
@@ -3,15 +3,22 @@ package com.stalkerfish.thecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+     tvInput = findViewById(R.id.tvInput)
+
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+        tvInput?.append("1")
     }
 }
