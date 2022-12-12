d398241b76f721d34351875f1f0c9e21771bd899
Allison Silva <silva.allison2002@gmail.com>
2022-12-12 16:17:12 -0300

    Add two more functions: onOperator and onOperatorAdded:
    onOperator only executes if the last digit is a number,
    and there is not a operator sign in the string.
    With that, we can assign what's inside the button without having something like:
    
     "4+/5"
    
    isOperatorAdded is an auxiliar function for onOperator,
    it returns true if the string of the TextView have any operator sign,
    and makes an exception for the minus sign,
    allowing us to do math with negative numbers.
    
    there is a "problem" with this. If we start the number with a minus,
    the code allow us to do things like:
    
    "-4+5/3*2-1"
    
    it is not wrong mathematically, but just happens if we start with a minus...
    
    Also changed the onClick function in the operator buttons to onOperator in the xml

--- app/src/main/java/com/stalkerfish/thecalculator/MainActivity.kt
+++ app/src/main/java/com/stalkerfish/thecalculator/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         tvInput = findViewById(R.id.tvInput)   // connects the variable to the actual UI element
+
     }
 
     private var lastWasNumeric: Boolean = true
@@ -31,12 +32,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDecimalPoint(view: View) {
-        if (lastWasNumeric && !lastWasDot) {
-            if ((tvInput?.text)?.contains(".") == false) {
+        if (lastWasNumeric && !lastWasDot) {        //  double checks if the last digit is a number and it is not a dot
+            if ((tvInput?.text)?.contains(".") == false) {      //  triple check if in the whole string has a dot
                 tvInput?.append(".")
                 lastWasDot = true
                 lastWasNumeric = false
             }
+        }
+    }
+
+    fun onOperator(view: View){
+        tvInput?.text?.let {    //  checks if tvInput is null
+
+            if(lastWasNumeric && !isOperatorAdded(it.toString())){      //  only executes if the last digit is a number and
+                tvInput?.append((view as Button).text)                  //  there is not a operator sign in the string
+                lastWasNumeric = false
+                lastWasDot = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean {
+
+        return if(value.startsWith("-")){   //  makes an exception for the minus sign,
+            false                                 // allowing us to do math with negative numbers
+        }
+
+        else {
+            value.contains("/")     //  return `true` if any of the following statements are satisfied
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
         }
     }
 }
