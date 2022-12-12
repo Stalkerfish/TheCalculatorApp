004cc45ddc6e68782becf29c90d31b587c5237ce
Allison Silva <silva.allison2002@gmail.com>
2022-12-12 13:34:26 -0300

    append the text in the button pressed to the top TextView, and adding comments

--- app/src/main/java/com/stalkerfish/thecalculator/MainActivity.kt
+++ app/src/main/java/com/stalkerfish/thecalculator/MainActivity.kt
@@ -3,22 +3,23 @@ package com.stalkerfish.thecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private var tvInput: TextView? = null
+    private var tvInput: TextView? = null   // variable to handle the top TextView, which shows the input of the user
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     tvInput = findViewById(R.id.tvInput)
+     tvInput = findViewById(R.id.tvInput)   // connects the variable to the actual UI element
 
     }
 
     fun onDigit(view: View){
-        tvInput?.append("1")
+        tvInput?.append((view as Button).text)  // append the text in the button pressed to tvInput
     }
 }
