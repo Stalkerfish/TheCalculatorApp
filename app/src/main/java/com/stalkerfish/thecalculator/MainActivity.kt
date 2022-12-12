b71cb45cdef6557f7c5b416360c9e4e6e1de8d8f
Allison Silva <silva.allison2002@gmail.com>
2022-12-12 14:42:52 -0300

    Add a new button (dot):
    later on, we want to use floating points, as we have divide operators
    so we need a dot button to make this functionality smoother to the user
    
    right now we have a problem: it is possible to add multiple dots in the number, iI don't know how to deal with that yet

--- app/src/main/java/com/stalkerfish/thecalculator/MainActivity.kt
+++ app/src/main/java/com/stalkerfish/thecalculator/MainActivity.kt
@@ -5,25 +5,39 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private var tvInput: TextView? = null   // variable to handle the top TextView, which shows the input of the user
+    private var tvInput: TextView? =
+        null   // variable to handle the top TextView, which shows the input of the user
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     tvInput = findViewById(R.id.tvInput)   // connects the variable to the actual UI element
+
+        tvInput = findViewById(R.id.tvInput)   // connects the variable to the actual UI element
 
     }
 
-    fun onDigit(view: View){
+    private var lastWasNumeric: Boolean = true
+    private var lastWasDot: Boolean = false
+
+    fun onDigit(view: View) {
         tvInput?.append((view as Button).text)  // append the text in the button pressed to tvInput
+        lastWasNumeric = true
+        lastWasDot = false
     }
 
-    fun onClear(view: View){    // This give functionality to the CLR button, inserting an empty string to tvInput
+    fun onClear(view: View) {    // This give functionality to the CLR button, inserting an empty string to tvInput
         tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: View) {
+        if (lastWasNumeric && !lastWasDot) {
+            tvInput?.append(".")
+            lastWasDot = true
+            lastWasNumeric = false
+        }
     }
 }
